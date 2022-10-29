ebc60d4a46b1aac095aaceaa027791bc526549c9
Samandar000 <samandarnfh.2020@gmail.com>
2022-10-29 23:30:11 +0500

    main

--- main.py
+++ main.py
@@ -1,52 +1,14 @@
-import requests
-from pprint import pprint
-TOKEN = '5643654386:AAGaxNP-8Kkwzi8Ko047p0BZBd3t6a0eIu4'
+import telegram
+import os
 
-def get_updates(TOKEN):
-    updates = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
-    updates = updates.json()
-    return updates
+TOKEN = os.environ['TOKEN']
 
-def get_lastupdate(updates):
-    last_update = updates['result'][-1]
-    chat_id = last_update['message']['chat']['id']
-    text = last_update['message']['text']
-    message_id = last_update['message']['message_id']
-    return chat_id,message_id,text
+bot = telegram.Bot(TOKEN)
+update = bot.getUpdates()[-1]
+chat_id = update.message.chat.id
+text = update.message.text
+photo = update.message.photo
+# img = 'https://c.ndtvimg.com/2020-08/h5mk7js_cat-generic_625x300_28_August_20.jpg'
+f = open('20546709.jpg','rb')
 
-def Region(chat_id):    
-    CITY = 'samarkand'
-    city = requests.get('https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid=29af92d9f93c8cf795be3ab8bb4f6776')
-    url= city.json()
-    data1 = {
-                'chat_id':chat_id,
-                'text':url,
-            }
-        
-    requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',json=data1)
-    
-def get_button(chat_id,text):
-        
-    button1 = {'text':'😺CAT'}
-    button2 = {'text':'🐶DOG'}
-
-    keyboard = [[button1,button2]]
-    
-    reply_markup = {'keyboard':keyboard,'resize_keyboard':True}
-    data = {
-            'chat_id':chat_id,
-            'text':text,
-            'reply_markup':reply_markup
-        }
-    requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',json=data)
-new_message = -1
-while True:
-    updates = get_updates(TOKEN)
-    lastupdate = get_lastupdate(updates)
-    chat_id,last_message_id,text= lastupdate
-
-    if new_message != last_message_id:
-        if text.lower()== 'samarkand' or 'bukhara':
-            Region(chat_id)
-            # get_button(chat_id)     
-        new_message = last_message_id
+bot.sendPhoto(chat_id, photo=f)
